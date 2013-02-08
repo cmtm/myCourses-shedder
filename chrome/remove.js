a6a467b000b090649db30a77a618a86c675306b5
Chris Morin <chris.morin2@gmail.com>
2013-02-07 20:16:21 -0500

    chrome extension

---
+++ chrome/remove.js
@@ -0,0 +1,8 @@
+document.querySelector('div.d_nb_c2').style.display = 'none';
+document.querySelector('div.d_nb_c3').style.display = 'none';
+document.querySelector('h1#d_page_title').style.display = 'none';
+document.querySelector('div.dtb_c').style.display = 'none';
+document.querySelector('div.dab_c').style.display = 'none';
+document.querySelector('h2.dhdg_1').style.display = 'none';
+document.querySelector('table.dsearch').style.display = 'none';
+
