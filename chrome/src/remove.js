cbd8717b57f33ffbc708014159c47702d475b243
Chris Morin <chris.morin2@gmail.com>
2013-02-25 03:34:56 -0500

    fixed gitignore issue

--- chrome/src/remove.js
+++ chrome/src/remove.js
@@ -1,28 +1,67 @@
-(function () {
-  document.querySelector('h1#d_page_title').style.display = 'none';
-  document.querySelector('div.dtb_c').style.display = 'none';
-  document.querySelector('div.dab_c').style.display = 'none';
-  document.querySelector('h2.dhdg_1').style.display = 'none';
-  document.querySelector('table.dsearch').style.display = 'none';
+(function shedder() {
 
-  var hasFrames = frames.length > 0;
+	// structure docs
+	// also hide objects if no frames
+	if (frames.length > 0) {
+		var navDoc = frames['nav'].document;
+		var contentDoc = frames['content'].document;
+		
+		// check for deeper frames
+		// detach sidebar if they exist
+		if (frames['content'].frames.length > 0) {
+			// improve discussion page
+			if (frames['content'].frames[0].name == 'FRAME_right') {
+				discussionDoc = frames['content'].frames[0].document;
+				discussionDoc.body.setAttribute("rows","100%");
+				discussionDoc.body.setAttribute("cols","50%,50%");
+			} else {
+				var sideBarDoc = frames['content'].frames['TOCFrame'].document;
+				contentDoc = frames['content'].frames['ContentFrame'].document;
+			}
+		}
+	} else {
+		var navDoc = document;
+		var contentDoc = document;
+		
+		// these objects only exist if there are no frames
+		try {
+			// page title
+			contentDoc.querySelector('h1#d_page_title').style.display = 'none';
+			// second title
+			contentDoc.querySelector('div.dtb_c').style.display = 'none';
+			contentDoc.querySelector('h2.dhdg_1').style.display = 'none';
 
-  var navDoc = hasFrames ? frames['nav'].document : document;
+		} catch(err) {}
+	}
+	
+	// set window title to course code
+	var title = navDoc.querySelector('div.d_nb_c3').querySelector('div.d_nb_mt').innerHTML;
+	
+	try {
+		document.title = title.match( /[A-Z]{4}-[0-9]{3}/ )[0].replace('-', ' ');
+	
+	
+		// move home button
+		var homeLink = navDoc.querySelectorAll('div.d_nb_c2 li')[0];
+		homeLink.querySelector('a span').innerHTML = 'My Courses';
+		
+		var ul = navDoc.querySelector('div.d_nb_c4 ul');
+		ul.insertBefore(homeLink, ul.firstChild);
+	} catch(err) {}
+	
+	// hide the whole top bar
+	navDoc.querySelector('div.d_nb_c2').style.display = 'none';
+	navDoc.querySelector('div.d_nb_c3').style.display = 'none';
+	
+	
 
-  var title = navDoc.querySelector('div.d_nb_c3').querySelector('div.d_nb_mt').innerHTML;
-  if (title !== undefined) {
-    document.title = title.match( /[A-Z]{4}-[0-9]{3}/ )[0].replace('-', ' ');
-  } else {
-    document.title = "My Courses";
-  }
-
-
-  var homeLink = navDoc.querySelectorAll('div.d_nb_c2 li')[0];
-  homeLink.querySelector('a span').innerHTML = 'My Courses';
-
-  var ul = navDoc.querySelector('div.d_nb_c4 ul');
-  ul.insertBefore(homeLink, ul.firstChild);
-
-  navDoc.querySelector('div.d_nb_c2').style.display = 'none';
-  navDoc.querySelector('div.d_nb_c3').style.display = 'none';
+	try{
+	// clean up sidebar & content
+	if ( typeof sideBarDoc != 'undefined') {
+		sideBarDoc.querySelectorAll('div.dco_c')[0].style.display = 'none';
+		sideBarDoc.querySelectorAll('div.dco_c')[1].style.display = 'none';
+		
+		contentDoc.querySelector('div.dab_c').style.display = 'none';
+	}
+	} catch (err) {}
 }());
